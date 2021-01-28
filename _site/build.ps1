9b9cc09a99ad96dcf495d71e178cf6097b987cbb
kderbyma_ <kyle@kderbyma.com>
2021-01-27 19:25:15 -0700

    updating content

--- _site/build.ps1
+++ _site/build.ps1
@@ -6,20 +6,20 @@ Set-Location $env:BUILD_PATH;
 . $env:BUILD_PATH/.env.ps1
 
 # Copy Environment Variables
-Copy-Item .env $env:BUILD_PATH/../../_calliope/.env
-Set-Location $env:BUILD_PATH/../../_calliope/app
+Copy-Item .env $env:BUILD_PATH/../_calliope/.env
+Set-Location $env:BUILD_PATH/../_calliope/app
 
 # Copy Content to Storage For Build
-Remove-Item $env:BUILD_PATH/../../_calliope/app/storage/* -Force -Recurse
-Copy-Item $env:BUILD_PATH/content/* $env:BUILD_PATH/../../_calliope/app/storage -Recurse -Force
+Remove-Item $env:BUILD_PATH/../_calliope/app/storage/* -Force -Recurse
+Copy-Item $env:BUILD_PATH/content/* $env:BUILD_PATH/../_calliope/app/storage -Recurse -Force
 
 yarn --production=false     # Install
-yarn deploy-site            # Build Site
+yarn deploy                 # Build Site
 yarn export                 # Export
 
 # Move Site Export
 Remove-Item $env:BUILD_PATH/output/* -Force -Recurse
-Set-Location $env:BUILD_PATH/../../_calliope
+Set-Location $env:BUILD_PATH/../_calliope
 Copy-Item output/site/* $env:BUILD_PATH/output -Recurse
 
 # Return to Start
