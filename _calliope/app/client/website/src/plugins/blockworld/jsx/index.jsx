dc8e3e1f7c7dd2c8d82ec68f006b03ac7734e946
kderbyma_ <kyle@kderbyma.com>
2021-04-11 17:56:00 -0600

    adding in blockwordl basics

---
+++ _calliope/app/client/website/src/plugins/blockworld/jsx/index.jsx
@@ -0,0 +1,49 @@
+/*                                                 *\
+** ----------------------------------------------- **
+**             Calliope - Site Generator   	       **
+** ----------------------------------------------- **
+**  Copyright (c) 2020-2021 - Kyle Derby MacInnis  **
+**                                                 **
+**    Any unauthorized distribution or transfer    **
+**       of this work is strictly prohibited.      **
+**                                                 **
+**               All Rights Reserved.              **
+** ----------------------------------------------- **
+\*                                                 */
+
+import React, { Component, useRef } from "react";
+import { collect } from "react-recollect";
+
+class BlockWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      networkString: props.networkString,
+      updated: Date.now(),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+    if (
+      JSON.stringify(this.props.networkString) !=
+      JSON.stringify(nextProps.networkString)
+    ) {
+      this.setState({
+        networkString: nextProps.networkString,
+        updated: Date.now(),
+      });
+    }
+  }
+
+  // TODO - Add Theme Injection Somehow (Or Wrapper)
+  render() {
+    const { diagram, updated } = this.state;
+    return (
+      <>
+        <Bitworld key={updated} networkString={this.networkString} />
+      </>
+    );
+  }
+}
+export default collect(MermaidDiagram);
